6e319010e9f001a119ce5d2c494f681ec248b044
Julian Lloyd <gremlin.ca@gmail.com>
2014-09-03 18:48:27 -0700

    Fix config.reset and refactor

--- scrollReveal.js
+++ scrollReveal.js
@@ -43,7 +43,7 @@ window.scrollReveal = (function( window ) {
        * check for a mobile browsers, and pull the plug if on
        * a mobile device and config.mobile is currently false
        */
-      if ( self.checkMobile() && !self.config.mobile ) {
+      if ( self.isMobile() && !self.config.mobile ) {
 
         return /* Goodbye… */
 
@@ -78,18 +78,13 @@ window.scrollReveal = (function( window ) {
        * 1, the element is considered in the viewport when it's fully visible
        * 0, the element is considered in the viewport as soon as it enters
        */
-      viewportFactor: 0.33,
+      viewportFactor: 0.4,
 
       /**
        * true, animations occur each time an element enters the viewport
        * false, animations occur only once
        */
       reset: false,
-
-      /**
-       * autostart triggers self.start() after first .init()
-       */
-      autostart: true,
 
       /**
        * true, init() is called during upon instantiation
@@ -108,52 +103,40 @@ window.scrollReveal = (function( window ) {
 
     /**
      * performs a fresh query of the DOM for data-sr attributes,
-     * and binds event listeners to the returned elements.
-     *
-     * TODO: De-couple the first animate() call, from the init() method.
+     * and binds event listeners to the returned elements
      */
     init: function() {
 
       var id
 
-      self.elems = Array.prototype.slice.call( self.docElem.querySelectorAll( '[data-sr]' ) )
+      self.elems = Array.prototype.slice.call( window.document.querySelectorAll( '[data-sr]' ) )
       self.elems.forEach(function ( el ) {
 
-        id = el.getAttribute( 'data-sr-id' )
-
+        id = self.serial++
         /**
-         * if id is undefined, assign a new one to self.data and
-         * to the DOM element (via the data-sr-style-id attribute)
+         * if no data-sr-id attribute is found, apply it and create a
+         * new member in self.data
          */
-        if ( !id ) {
+        if ( !el.getAttribute( 'data-sr-id' ) ) {
 
-          id = self.serial++
-
-
-          self.data[ id ]    = {}
-          self.data[ id ].id = id
-
+          self.data[ id ] = {}
           el.setAttribute( 'data-sr-id', id )
 
-        } else return
+        } else {
+
+          /**
+           * Otherwise, Skip this element’s setup...
+           */
+          return
+        }
 
         /**
          * if no style property has been set, create one using the value from
-         * the element's existing style tag — otherwise set to false
+         * the element's existing style tag — otherwise initialise an empty string
          */
-        if ( !self.data[ id ].style ) {
+        if ( typeof self.data[ id ].style === 'undefined' ) {
 
-          self.data[ id ].style = el.getAttribute( 'style' )
-
-          if ( self.data[ id ].style ) {
-
-            self.data[ id ].style += ''
-
-          } else {
-
-            self.data[ id ].style = ''
-
-          }
+          self.data[ id ].style = el.getAttribute( 'style' ) + ';'
 
         }
 
@@ -178,12 +161,16 @@ window.scrollReveal = (function( window ) {
 
         var id
           , css
-          , reveal
+          , data
 
         /**
          * first, let’s skip any (non-reseting) elements that have completed their reveal
          */
-        if ( el.getAttribute( 'data-sr-complete' ) ) { return }
+        if ( el.getAttribute( 'data-sr-complete' ) ) {
+
+          return
+
+        }
 
         /**
          * find the elements id, and set `el` to its corresponding self.data object
@@ -192,21 +179,20 @@ window.scrollReveal = (function( window ) {
 
         if ( id ) {
 
-          reveal = self.data[ id ]
-
+          data = self.data[ id ]
 
           /**
            * now lets check for cached styles, and generated any that are missing
            */
-          if ( !reveal.css ) {
+          if ( !data.css ) {
 
-            reveal.css = self.createStyles( el, reveal )
+            data.css = self.createStyles( el, data )
           }
 
-          if ( !el.getAttribute( 'data-sr-initialized' ) ) {
+          if ( !el.getAttribute( 'data-sr-init' ) ) {
 
-            el.setAttribute( 'style', reveal.style + reveal.css.initial )
-            el.setAttribute( 'data-sr-initialized', true )
+            el.setAttribute( 'style', data.style + data.css.initial )
+            el.setAttribute( 'data-sr-init', true )
 
           }
 
@@ -215,9 +201,9 @@ window.scrollReveal = (function( window ) {
            */
           if ( !self.isElementInViewport( el, self.config.viewportFactor ) ) {
 
-            if ( reveal.reset ) {
+            if ( data.reset ) {
 
-              el.setAttribute( 'style', reveal.style + reveal.css.initial + reveal.css.reset )
+              el.setAttribute( 'style', data.style + data.css.initial + data.css.reset )
 
             }
 
@@ -230,19 +216,19 @@ window.scrollReveal = (function( window ) {
            */
           if ( self.isElementInViewport( el, self.config.viewportFactor ) ) {
 
-            el.setAttribute( 'style', reveal.style + reveal.css.target + reveal.css.transition )
+            el.setAttribute( 'style', data.style + data.css.target + data.css.transition )
 
             /**
              * and if reset is disabled for this element, restore the style attribute
              * to it’s pre-scrollReveal state after the animation completes
              */
-            if ( !reveal.reset ) {
+            if ( !data.reset ) {
 
               setTimeout(function () {
 
-                if ( reveal.style !== '' ) {
+                if ( data.style !== '' ) {
 
-                  el.setAttribute( 'style', reveal.style )
+                  el.setAttribute( 'style', data.style )
 
                 } else {
 
@@ -251,7 +237,7 @@ window.scrollReveal = (function( window ) {
 
                 el.setAttribute( 'data-sr-complete', true ) /* complete */
 
-              }, reveal.css.totalDuration )
+              }, data.css.totalDuration )
             }
 
             return
@@ -353,15 +339,12 @@ window.scrollReveal = (function( window ) {
       /**
        * if no reset keyword is found, assign the value specified in the config
        */
-      if ( typeof parsed.reset === 'undefined' && parsed.reset != null ) {
-
-        parsed.reset = self.config.reset
-      }
+      if ( typeof parsed.reset === 'undefined' ) parsed.reset = self.config.reset
 
       return parsed
     },
 
-    createStyles: function( el, reveal ) {
+    createStyles: function( el, data ) {
 
       var parsed
         , enter
@@ -382,7 +365,7 @@ window.scrollReveal = (function( window ) {
        * parse for keywords, and update our data model's reset config
        */
       parsed = self.parse( el )
-      reveal.reset = parsed.reset
+      data.reset = parsed.reset
 
       /**
        * convert the vector origin to a CSS-friendly axis
@@ -449,9 +432,9 @@ window.scrollReveal = (function( window ) {
        * Want to disable delay on mobile devices? It might provide a better UX
        * considering the animations are paused during scroll… uncomment the line below
        */
-      //if ( self.checkMobile() && self.config.mobile ) { delay = 0; }
+      //if ( self.isMobile() && self.config.mobile ) { delay = 0; }
 
-      transition = '-webkit-transition: -webkit-transform ' + dur + ' ' + easing + ' ' + delay + ',  opacity ' + dur + ' ' + easing + ' ' + delay + ';' +
+      transition = '-webkit-transition: -webkit-transform ' + dur + ' ' + easing + ' ' + delay + ', opacity ' + dur + ' ' + easing + ' ' + delay + ';' +
                            'transition: transform '         + dur + ' ' + easing + ' ' + delay + ', opacity ' + dur + ' ' + easing + ' ' + delay + ';' +
                   '-webkit-perspective: 1000;' +
           '-webkit-backface-visibility: hidden;'
@@ -459,8 +442,8 @@ window.scrollReveal = (function( window ) {
       /**
        * create a duplicate transition style, but with no delay for better exit animation UX
        */
-      reset      = '-webkit-transition: -webkit-transform ' + dur + ' ' + easing + ' 0s,  opacity ' + dur + ' ' + easing + ' ' + delay + ';' +
-                           'transition: transform '         + dur + ' ' + easing + ' 0s,  opacity ' + dur + ' ' + easing + ' ' + delay + ';' +
+      reset      = '-webkit-transition: -webkit-transform ' + dur + ' ' + easing + ' 0s, opacity ' + dur + ' ' + easing + ' ' + delay + ';' +
+                           'transition: transform '         + dur + ' ' + easing + ' 0s, opacity ' + dur + ' ' + easing + ' ' + delay + ';' +
                   '-webkit-perspective: 1000;' +
           '-webkit-backface-visibility: hidden;'
 
@@ -548,9 +531,7 @@ window.scrollReveal = (function( window ) {
     isElementInViewport: function( el, viewportFactor ) {
 
       if ( self.docElem == window.document.documentElement )
-
-        var scrolled     = window.pageYOffset
-
+           var scrolled  = window.pageYOffset
       else var scrolled  = self.docElem.scrollTop + self.docElem.offsetTop
 
       var elHeight       = el.offsetHeight
@@ -565,13 +546,11 @@ window.scrollReveal = (function( window ) {
           || ( el.currentStyle? el.currentStyle : window.getComputedStyle( el, null ) ).position == 'fixed'
     },
 
-    checkMobile: function() {
+    isMobile: function() {
 
-      var result = false
+      var agent = navigator.userAgent||navigator.vendor||window.opera
 
-      // (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))result = true})(navigator.userAgent||navigator.vendor||window.opera);
-
-      return result
+      return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test( agent )||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test( agent.substr( 0, 4 ))) ? true : false
     },
 
     extend: function( a, b ) {
